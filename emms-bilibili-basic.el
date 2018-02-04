a03e829ab4a66fcb58946088174798ed28d1dfa8
stardiviner <numbchild@gmail.com>
2018-02-05 00:14:24 +0800

    decompose main file into separate basic, request, bookmark files
    - Setup a main function to Bilibili entries.

---
+++ emms-bilibili-basic.el
@@ -0,0 +1,64 @@
+;;; emms-bilibili-basic.el --- provide basic API for other emms-bilibili files.
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+(require 'emms)
+(require 'emms-browser)
+(require 'emms-source-playlist)
+
+(defgroup emms-bilibili nil
+  "Play your Bilibili favourite videos in EMMS."
+  :prefix "emms-bilibili-"
+  :group 'emms)
+
+
+(defun emms-bilibili-insert-track (element)
+  "Create track and insert `ELEMENT' into emms-playlist."
+  (let* ((title (alist-get 'title element))
+         (artist (alist-get 'name (alist-get 'owner element)))
+         (duration (alist-get 'duration element))
+         (url (emms-bilibili-generate-video-url (alist-get 'aid element)))
+         (state (alist-get 'state element))
+         (track (emms-track 'url url)))
+    (when (>= state 0)
+      (emms-track-set track 'info-title title)
+      (emms-track-set track 'info-artist artist)
+      (emms-track-set track 'info-playing-time duration)
+      (with-current-emms-playlist
+        (emms-playlist-insert-track track)))))
+
+(defvar emms-bilibili-playlist-alist nil
+  "An alist storing `emms-bilibili' playlist.")
+
+(defun emms-bilibili-sync-playlist (entry)
+  "Dispatch Bilibili playlist to collect entries to the EMMS Playlist buffer.
+
+Prompt for a Bilibili entry like bookmark to sync playlist."
+  (cl-case (intern entry)
+    ('bookmark
+     (emms-bilibili-bookmark-sync))
+    ('user-space
+     ;; TODO:
+     (message (format "%s currently is not supported by emms-bilibili yet.") entry)
+     )
+    ('bangumi
+     ;; TODO:
+     (message (format "%s currently is not supported by emms-bilibili yet.") entry)
+     )))
+
+;; insert tracks into EMMS Playlist after response received.
+(defvar emms-bilibili-response-received-hook nil)
+(add-hook 'emms-bilibili-response-received-hook
+          (lambda () (mapcar 'emms-bilibili-insert-track emms-bilibili-playlist-alist)))
+(add-hook 'emms-bilibili-response-received-hook
+          (lambda () (message "EMMS Bilibili fetch playlist done.")))
+
+
+
+(provide 'emms-bilibili-basic)
+
+;;; emms-bilibili-basic.el ends here
---
+++ emms-bilibili-request.el
@@ -0,0 +1,60 @@
+;;; emms-bilibili-request.el --- Bilibili request wrapper.
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+(require 'url)
+(require 'json)
+(require 'emms-bilibili-basic)
+
+;; ssl magic
+;; (setq tls-program '("openssl s_client -connect %h:%p -no_ssl2 -ign_eof"))
+
+(defcustom emms-bilibili-mid nil
+  "User mid."
+  :type 'number
+  :group 'emms-bilibili)
+
+(defun emms-bilibili-get-mid ()
+  "Prompt user for mid."
+  (unless (not (null emms-bilibili-mid))
+    (browse-url "https://space.bilibili.com/")
+    (setq emms-bilibili-mid (read-from-minibuffer "Input your Bilibili user mid number: "))))
+
+(defun emms-bilibili-generate-video-url (aid)
+  "Generate video URL from `AID'."
+  (format "https://www.bilibili.com/video/av%d/" aid))
+
+(defun emms-bilibili-generate-bangumi-url (bangumi)
+  "Generate Bangumi URL from `BANGUMI'."
+  (format "https://bangumi.bilibili.com/anime/%d/" bangumi))
+
+(defun emms-bilibili-url-clean-response-buffer ()
+  "Delete header from response buffer."
+  (goto-char (point-min))
+  (re-search-forward "^$")
+  (delete-region (point) (point-min))
+  (delete-blank-lines))
+
+(defun emms-bilibili-response-json-parse (response)
+  "Parse the `RESPONSE' to get JSON object data."
+  (let ((response-buffer (current-buffer))
+        (json-array-type 'list))
+    (with-current-buffer response-buffer
+      (emms-bilibili-url-clean-response-buffer)
+      (let* ((json-raw (json-read-from-string
+                        (decode-coding-string (buffer-string) 'utf-8)))
+             (pagecount (alist-get 'pagecount (alist-get 'data json-raw)))
+             (current-page (alist-get 'page (alist-get 'data json-raw)))
+             (data (assoc 'data json-raw))
+             )
+        data))))
+
+
+
+(provide 'emms-bilibili-request)
+
+;;; emms-bilibili-request.el ends here
